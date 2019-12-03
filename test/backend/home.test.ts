37644b48bb15b67e19f78ebae3a8c9a64ab073a9
Shabab Karim <skarim222@gmail.com>
2019-12-03 06:25:20 -0600

    Added testing for backend with simple test case

---
+++ test/backend/home.test.ts
@@ -0,0 +1,10 @@
+import request from "supertest";
+import app from "../../src/app";
+
+describe("GET /", () => {
+	it("returns a 200 ok response", () => {
+		return request(app)
+			.get("/")
+			.expect(200);
+	});
+});
