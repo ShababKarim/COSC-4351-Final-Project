570b37f5b14943d43636bbc841437dfa1f0598f7
Bryan Ellis <bryan.ellis.t@gmail.com>
2019-12-04 02:41:11 -0600

    styling updates

--- src/public/src/components/dash/links.js
+++ src/public/src/components/dash/links.js
@@ -1,77 +1,77 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import { LINKROLES } from "../../util";
 
 const drawerWidth = 240;
 const useStyles = makeStyles(theme => ({
-    root: {
-        display: 'flex'
-    },
-    appBar: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth + 30
-    },
-    card: {
-        backgroundColor: 'primary'
-    },
-    content: {
-        textAlign: 'center'
-    },
-    page: {
-        marginLeft: drawerWidth + 30,
-        marginTop: 50
-    }
+  root: {
+    display: "flex"
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth + 30
+  },
+  card: {
+    backgroundColor: "primary"
+  },
+  content: {
+    textAlign: "center"
+  },
+  page: {
+    marginLeft: drawerWidth + 30,
+    marginTop: 50
+  }
 }));
 
 const Links = props => {
-    const classes = useStyles();
-
-    return (
-        <div id="more" className={classes.page}>
-            <div>
-                <GridList cols={4} className={classes.page}>
-                    {props.links.map(({ url, _id }) => {
-                        return (
-                            <GridListTile key={_id}>
-                                <Card
-                                    raised={true}
-                                    style={{
-                                        maxWidth: 250,
-                                        backgroundColor: '#D32F2F',
-                                        color: 'white'
-                                    }}
-                                >
-                                    <CardContent className={classes.content}>
-                                        <Typography variant="h5">
-                                            {url}
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button
-                                            size="large"
-                                            fullWidth
-                                            style={{
-                                                color: 'white'
-                                            }}
-                                        >
-                                            <ArrowForwardIcon></ArrowForwardIcon>
-                                        </Button>
-                                    </CardActions>
-                                </Card>
-                            </GridListTile>
-                        );
-                    })}
-                </GridList>
-            </div>
-        </div>
-    );
+  const classes = useStyles();
+  console.log(props.links);
+  return (
+    <div id="more" className={classes.page}>
+      <div>
+        <GridList cols={4} className={classes.page}>
+          {props.links.map(({ url, adminType }) => {
+            return (
+              <GridListTile key={adminType}>
+                <Card
+                  raised={true}
+                  style={{
+                    maxWidth: 250,
+                    backgroundColor: "#D32F2F",
+                    color: "white"
+                  }}
+                >
+                  <CardContent className={classes.content}>
+                    <Typography variant="h5">{url}</Typography>
+                    <Typography variant="body2">{LINKROLES[adminType]}</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      size="large"
+                      fullWidth
+                      style={{
+                        color: "white"
+                      }}
+                    >
+                      <ArrowForwardIcon></ArrowForwardIcon>
+                    </Button>
+                  </CardActions>
+                </Card>
+              </GridListTile>
+            );
+          })}
+        </GridList>
+      </div>
+    </div>
+  );
 };
 
 export default Links;
--- src/public/src/util.js
+++ src/public/src/util.js
@@ -1,49 +1,57 @@
 export const ROLES = [
-	['ADMIN', 'Admin'],
-	['SALES_ADMIN', 'Sales Admin'],
-	['ENGG_ADMIN', 'Eng. Admin'],
-	['HR_ADMIN', 'HR Admin'],
-	['FINANCE_ADMIN', 'Finance Admin']
+  ["ADMIN", "Admin"],
+  ["SALES_ADMIN", "Sales Admin"],
+  ["ENGG_ADMIN", "Eng. Admin"],
+  ["HR_ADMIN", "HR Admin"],
+  ["FINANCE_ADMIN", "Finance Admin"]
 ];
 
+export const LINKROLES = {
+  ADMIN: "Admin",
+  SALES_ADMIN: "Sales Admin",
+  ENGG_ADMIN: "Eng. Admin",
+  HR_ADMIN: "HR Admin",
+  FINANCE_ADMIN: "Finance Admin"
+};
+
 export async function changePendingStatus(email, adminType, endpoint) {
-	try {
-		const response = await fetch('http://localhost:5000/' + endpoint, {
-			method: 'POST',
-			mode: 'cors',
-			headers: {
-				'content-type': 'application/json',
-				'x-access-token': sessionStorage.getItem('x-auth-token')
-			},
-			body: JSON.stringify({
-				email,
-				adminType
-			})
-		});
-		const res = await response.text();
-		if (response.status !== 200) throw new Error(res);
-	} catch (err) {
-		alert(err);
-	}
+  try {
+    const response = await fetch("http://localhost:5000/" + endpoint, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "content-type": "application/json",
+        "x-access-token": sessionStorage.getItem("x-auth-token")
+      },
+      body: JSON.stringify({
+        email,
+        adminType
+      })
+    });
+    const res = await response.text();
+    if (response.status !== 200) throw new Error(res);
+  } catch (err) {
+    alert(err);
+  }
 }
 
 export async function changeAdminStatus(email, adminType, endpoint) {
-	try {
-		const response = await fetch('http://localhost:5000/' + endpoint, {
-			method: 'POST',
-			mode: 'cors',
-			headers: {
-				'content-type': 'application/json',
-				'x-access-token': sessionStorage.getItem('x-auth-token')
-			},
-			body: JSON.stringify({
-				email,
-				adminType
-			})
-		});
-		const res = await response.text();
-		if (response.status !== 200) throw new Error(res);
-	} catch (err) {
-		alert(err);
-	}
+  try {
+    const response = await fetch("http://localhost:5000/" + endpoint, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "content-type": "application/json",
+        "x-access-token": sessionStorage.getItem("x-auth-token")
+      },
+      body: JSON.stringify({
+        email,
+        adminType
+      })
+    });
+    const res = await response.text();
+    if (response.status !== 200) throw new Error(res);
+  } catch (err) {
+    alert(err);
+  }
 }
