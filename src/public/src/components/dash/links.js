073aa459edd251cdc1d07bc5134ec345a033590a
Bryan Ellis <bryan.ellis.t@gmail.com>
2019-12-04 12:41:13 -0600

    card styling

--- src/public/src/components/dash/links.js
+++ src/public/src/components/dash/links.js
@@ -12,75 +12,76 @@ import { LINKROLES } from '../../util';
 
 const drawerWidth = 240;
 const useStyles = makeStyles(theme => ({
-	root: {
-		display: 'flex'
-	},
-	appBar: {
-		width: `calc(100% - ${drawerWidth}px)`,
-		marginLeft: drawerWidth + 30
-	},
-	card: {
-		backgroundColor: 'primary'
-	},
-	content: {
-		textAlign: 'center'
-	},
-	page: {
-		marginLeft: drawerWidth + 30,
-		marginTop: 50
-	}
+    root: {
+        display: 'flex'
+    },
+    appBar: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth + 30
+    },
+    card: {
+        backgroundColor: 'primary'
+    },
+    content: {
+        textAlign: 'center'
+    },
+    page: {
+        marginLeft: drawerWidth + 30,
+        marginTop: 50
+    }
 }));
 
 const Links = props => {
-	const classes = useStyles();
-	return (
-		<div id='more' className={classes.page}>
-			<div>
-				<GridList cols={4} className={classes.page}>
-					{props.links.map(({ url, adminType }) => {
-						return (
-							<GridListTile key={adminType}>
-								<Card
-									raised={true}
-									style={{
-										maxWidth: 250,
-										backgroundColor: '#D32F2F',
-										color: 'white'
-									}}
-								>
-									<CardContent className={classes.content}>
-										<Typography variant='h5'>
-											{url}
-										</Typography>
-										<Typography variant='body2'>
-											{LINKROLES[adminType]}
-										</Typography>
-									</CardContent>
-									<CardActions>
-										<Button
-											size='large'
-											fullWidth
-											style={{
-												color: 'white'
-											}}
-											onClick={() =>
-												window.open(
-													'http://' + url,
-													'_blank'
-												)
-											}
-										>
-											<ArrowForwardIcon></ArrowForwardIcon>
-										</Button>
-									</CardActions>
-								</Card>
-							</GridListTile>
-						);
-					})}
-				</GridList>
-			</div>
-		</div>
-	);
+    const classes = useStyles();
+    return (
+        <div id="more" className={classes.page}>
+            <div>
+                <GridList cols={4} className={classes.page}>
+                    {props.links.map(({ url, adminType }) => {
+                        return (
+                            <GridListTile key={adminType}>
+                                <Card
+                                    raised={true}
+                                    style={{
+                                        maxWidth: 300,
+                                        maxHeight: 150,
+                                        backgroundColor: '#D32F2F',
+                                        color: 'white'
+                                    }}
+                                >
+                                    <CardContent className={classes.content}>
+                                        <Typography variant="h5">
+                                            {url}
+                                        </Typography>
+                                        <Typography variant="body2">
+                                            {LINKROLES[adminType]}
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button
+                                            size="large"
+                                            fullWidth
+                                            style={{
+                                                color: 'white'
+                                            }}
+                                            onClick={() =>
+                                                window.open(
+                                                    'http://' + url,
+                                                    '_blank'
+                                                )
+                                            }
+                                        >
+                                            <ArrowForwardIcon></ArrowForwardIcon>
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </GridListTile>
+                        );
+                    })}
+                </GridList>
+            </div>
+        </div>
+    );
 };
 
 export default Links;
