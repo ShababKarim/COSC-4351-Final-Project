b38f3af7b5820e69a1f1254d0f3dc8d4aa71448b
Shabab Karim <skarim222@gmail.com>
2019-12-03 19:37:11 -0600

    Added current user panel

--- src/public/src/components/AdminPanel/CurrentUsers.js
+++ src/public/src/components/AdminPanel/CurrentUsers.js
@@ -1,9 +1,77 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
 
-const CurrentUsers = props => {
+import { ROLES, changeAdminStatus } from '../../util';
+
+const CurrentUsers = ({ current, setCurrent }) => {
+	const [formValue, setFormValue] = useState({ email: '', adminType: '' });
+
+	const handleChange = useCallback((select, value) => {
+		setFormValue({ ...formValue, [select]: value });
+	});
+
+	const handleRevokeorRemoval = useCallback(async changeType => {
+		let endpoint;
+		switch (changeType) {
+			case 'Revoke':
+				endpoint = 'api/update/admin-revoke';
+				break;
+			case 'Remove':
+				endpoint = 'api/remove/admin';
+				break;
+			default:
+				endpoint = 'api/update/admin';
+		}
+		await changeAdminStatus(formValue.email, formValue.adminType, endpoint);
+		setFormValue({ email: '', adminType: '' });
+	});
+
 	return (
 		<div>
-			<p>This</p>
+			<FormControl>
+				<Select
+					id='user-select'
+					value={formValue.email}
+					onChange={e => handleChange('email', e.target.value)}
+				>
+					<MenuItem value={''}></MenuItem>
+					{current.map(({ _id, email }) => (
+						<MenuItem key={_id} value={email}>
+							{email}
+						</MenuItem>
+					))}
+				</Select>
+				<Select
+					id='admin-select'
+					value={formValue.adminType}
+					onChange={e => handleChange('adminType', e.target.value)}
+				>
+					<MenuItem value={''}></MenuItem>
+					{ROLES.map((role, index) => (
+						<MenuItem key={index} value={role[0]}>
+							{role[1]}
+						</MenuItem>
+					))}
+				</Select>
+				<Button
+					color='primary'
+					onClick={() => handleRevokeorRemoval('Modify')}
+				>
+					Change
+				</Button>
+				<Button
+					color='secondary'
+					onClick={() => handleRevokeorRemoval('Revoke')}
+				>
+					Revoke
+				</Button>
+				<Button onClick={() => handleRevokeorRemoval('Remove')}>
+					Remove
+				</Button>
+			</FormControl>
 		</div>
 	);
 };
--- src/public/src/util.js
+++ src/public/src/util.js
@@ -26,3 +26,24 @@ export async function changePendingStatus(email, adminType, endpoint) {
 		alert(err);
 	}
 }
+
+export async function changeAdminStatus(email, adminType, endpoint) {
+	try {
+		const response = await fetch('http://localhost:5000/' + endpoint, {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'content-type': 'application/json',
+				'x-access-token': sessionStorage.getItem('x-auth-token')
+			},
+			body: JSON.stringify({
+				email,
+				adminType
+			})
+		});
+		const res = await response.text();
+		if (response.status !== 200) throw new Error(res);
+	} catch (err) {
+		alert(err);
+	}
+}
