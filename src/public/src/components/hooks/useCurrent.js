810e1fee37f224e112878f28c972bc8e94ac28ca
Bryan Ellis <bryan.ellis.t@gmail.com>
2019-12-03 23:39:35 -0600

    fixed initial login fail

--- src/public/src/components/hooks/useCurrent.js
+++ src/public/src/components/hooks/useCurrent.js
@@ -1,34 +1,30 @@
-import { useState, useEffect } from 'react';
-
-const fetchOptions = {
-	method: 'GET',
-	mode: 'cors',
-	headers: {
-		'x-access-token': sessionStorage.getItem('x-auth-token')
-	}
-};
+import { useState, useEffect } from "react";
 
 const useCurrent = () => {
-	const [current, setCurrent] = useState([]);
+  const fetchOptions = {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "x-access-token": sessionStorage.getItem("x-auth-token")
+    }
+  };
+  const [current, setCurrent] = useState([]);
 
-	useEffect(() => {
-		async function getCurrent() {
-			try {
-				const response = await fetch(
-					'http://localhost:5000/api/current',
-					fetchOptions
-				);
-				const res = await response.json();
-				if (response.status !== 200) throw new Error(res);
-				setCurrent(res);
-			} catch (err) {
-				alert(err);
-			}
-		}
-		getCurrent();
-	}, []);
+  useEffect(() => {
+    async function getCurrent() {
+      try {
+        const response = await fetch("http://localhost:5000/api/current", fetchOptions);
+        const res = await response.json();
+        if (response.status !== 200) throw new Error(res);
+        setCurrent(res);
+      } catch (err) {
+        alert(err);
+      }
+    }
+    getCurrent();
+  }, []);
 
-	return [current, setCurrent];
+  return [current, setCurrent];
 };
 
 export default useCurrent;
--- src/public/src/pages/dashboard.js
+++ src/public/src/pages/dashboard.js
@@ -1,48 +1,42 @@
-import React, { useState, useContext, useEffect } from 'react';
-import AuthContext from '../context';
+import React, { useState, useContext, useEffect } from "react";
+import AuthContext from "../context";
 
-import NavBar from '../components/dash/navbar';
-import HomeDrawer from '../components/dash/homeDrawer';
-import Links from '../components/dash/links';
-import SuperAdminTabs from '../components/superadmin/superadmintabs';
-
-const fetchOptions = {
-	method: 'GET',
-	mode: 'cors',
-	headers: {
-		'x-access-token': sessionStorage.getItem('x-auth-token')
-	}
-};
+import NavBar from "../components/dash/navbar";
+import HomeDrawer from "../components/dash/homeDrawer";
+import Links from "../components/dash/links";
+import SuperAdminTabs from "../components/superadmin/superadmintabs";
 
 const Dashboard = () => {
-	const { auth } = useContext(AuthContext);
-	const [links, setLinks] = useState([]);
-	useEffect(() => {
-		async function getLinks() {
-			try {
-				const response = await fetch(
-					'http://localhost:5000/api/links',
-					fetchOptions
-				);
-				const res = await response.json();
-				if (response.status !== 200) throw new Error(res);
-				setLinks(res);
-			} catch (err) {
-				alert(err);
-			}
-		}
-		getLinks();
-	}, []);
-	return (
-		<div>
-			<NavBar />
-			<HomeDrawer />
-			<Links links={links} />
-			{auth && auth.adminType === 'SUPER_ADMIN' ? (
-				<SuperAdminTabs links={links} />
-			) : null}
-		</div>
-	);
+  const fetchOptions = {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "x-access-token": sessionStorage.getItem("x-auth-token")
+    }
+  };
+  const { auth } = useContext(AuthContext);
+  const [links, setLinks] = useState([]);
+  useEffect(() => {
+    async function getLinks() {
+      try {
+        const response = await fetch("http://localhost:5000/api/links", fetchOptions);
+        const res = await response.json();
+        if (response.status !== 200) throw new Error(res);
+        setLinks(res);
+      } catch (err) {
+        alert(err);
+      }
+    }
+    getLinks();
+  }, []);
+  return (
+    <div>
+      <NavBar />
+      <HomeDrawer />
+      <Links links={links} />
+      {auth && auth.adminType === "SUPER_ADMIN" ? <SuperAdminTabs links={links} /> : null}
+    </div>
+  );
 };
 
 export default Dashboard;
